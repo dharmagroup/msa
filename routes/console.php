d985cfc1fcb8730a84079878ac51bf336b86d307
Abdul Rohim <abdulrohim739@gmail.com>
2024-10-31 08:00:24 +0700

    update

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\Modules\Manufacture\Andons;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('repair:start-automatic', function () {
-    $this->comment(Inspiring::quote());
+    $andons = new Andons();
+    $andons->_create_making_repair_automatic();
+    $this->info('Tugas berhasil dijalankan!');
 })->purpose('Display an inspiring quote')->everySecond();
